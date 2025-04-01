625f9da5645a555c70f9b8593b1e49dc1910a29d
Dariush Tasdighi <dariusht@gmail.com>
2025-04-01 19:04:36 +0330

    Version 0.0.3

---
+++ src/Core/Domain/Features/Cms/Slide.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Domain.Seedwork;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Features.Cms;
+
+public class Slide(string title, string imageUrl) : Entity(),
+	Dtat.Seedwork.Abstractions.IEntityHasIsActive,
+	Dtat.Seedwork.Abstractions.IEntityHasOrdering,
+	Dtat.Seedwork.Abstractions.IEntityHasUpdateDateTime
+{
+	/// <summary>
+	/// وضعیت
+	/// </summary>
+	//[System.ComponentModel.DataAnnotations.Display
+	//	(ResourceType = typeof(Resources.DataDictionary),
+	//	Name = nameof(Resources.DataDictionary.IsActive))]
+	public bool IsActive { get; set; }
+
+	/// <summary>
+	/// وضعیت
+	/// </summary>
+	//[System.ComponentModel.DataAnnotations.Display
+	//	(ResourceType = typeof(Resources.DataDictionary),
+	//	Name = nameof(Resources.DataDictionary.OpenUrlInNewWindow))]
+	public bool OpenUrlInNewWindow { get; set; }
+
+	/// <summary>
+	/// فاصله زمانی نمایش
+	/// </summary>
+	//[System.ComponentModel.DataAnnotations.Display
+	//	(ResourceType = typeof(Resources.DataDictionary),
+	//	Name = nameof(Resources.DataDictionary.Interval))]
+
+	[System.ComponentModel.DataAnnotations.Required
+		(AllowEmptyStrings = false,
+		ErrorMessageResourceType = typeof(Resources.Messages.Validations),
+		ErrorMessageResourceName = nameof(Resources.Messages.Validations.Required))]
+
+	[System.ComponentModel.DataAnnotations.Range
+		(minimum: 100, maximum: 60_000,
+		ErrorMessageResourceType = typeof(Resources.Messages.Validations),
+		ErrorMessageResourceName = nameof(Resources.Messages.Validations.Range))]
+	public int Interval { get; set; } = 5_000;
+
+	/// <summary>
+	/// چیدمان
+	/// </summary>
+	//[System.ComponentModel.DataAnnotations.Display
+	//	(ResourceType = typeof(Resources.DataDictionary),
+	//	Name = nameof(Resources.DataDictionary.Ordering))]
+
+	[System.ComponentModel.DataAnnotations.Required
+		(AllowEmptyStrings = false,
+		ErrorMessageResourceType = typeof(Resources.Messages.Validations),
+		ErrorMessageResourceName = nameof(Resources.Messages.Validations.Required))]
+
+	[System.ComponentModel.DataAnnotations.Range
+		(minimum: 1, maximum: 100_000,
+		ErrorMessageResourceType = typeof(Resources.Messages.Validations),
+		ErrorMessageResourceName = nameof(Resources.Messages.Validations.Range))]
+	public long Ordering { get; set; } = 10_000;
+
+	/// <summary>
+	/// عنوان
+	/// </summary>
+	[System.ComponentModel.DataAnnotations.Display
+		(ResourceType = typeof(Resources.DataDictionary),
+		Name = nameof(Resources.DataDictionary.Title))]
+
+	[System.ComponentModel.DataAnnotations.Required
+		(AllowEmptyStrings = false,
+		ErrorMessageResourceType = typeof(Resources.Messages.Validations),
+		ErrorMessageResourceName = nameof(Resources.Messages.Validations.Required))]
+
+	//[System.ComponentModel.DataAnnotations.MaxLength
+	//	(length: Constants.MaxLength.Title,
+	//	ErrorMessageResourceType = typeof(Resources.Messages.Validations),
+	//	ErrorMessageResourceName = nameof(Resources.Messages.Validations.MaxLength))]
+	public string Title { get; set; } = title;
+
+	/// <summary>
+	/// توضیحات
+	/// </summary>
+	//[System.ComponentModel.DataAnnotations.Display
+	//	(ResourceType = typeof(Resources.DataDictionary),
+	//	Name = nameof(Resources.DataDictionary.Caption))]
+	public string? Caption { get; set; }
+
+	/// <summary>
+	/// لینک تصویر اسلاید
+	/// </summary>
+	//[System.ComponentModel.DataAnnotations.Display
+	//	(ResourceType = typeof(Resources.DataDictionary),
+	//	Name = nameof(Resources.DataDictionary.ImageUrl))]
+
+	[System.ComponentModel.DataAnnotations.Required
+		(AllowEmptyStrings = false,
+		ErrorMessageResourceType = typeof(Resources.Messages.Validations),
+		ErrorMessageResourceName = nameof(Resources.Messages.Validations.Required))]
+	public string ImageUrl { get; set; } = imageUrl;
+
+	/// <summary>
+	/// توضیحات
+	/// </summary>
+	[System.ComponentModel.DataAnnotations.Display
+		(ResourceType = typeof(Resources.DataDictionary),
+		Name = nameof(Resources.DataDictionary.Description))]
+	public string? Description { get; set; }
+
+	/// <summary>
+	/// لینک مقصد اسلاید
+	/// </summary>
+	//[System.ComponentModel.DataAnnotations.Display
+	//	(ResourceType = typeof(Resources.DataDictionary),
+	//	Name = nameof(Resources.DataDictionary.NavigationUrl))]
+	public string? NavigationUrl { get; set; }
+
+	[DatabaseGenerated(databaseGeneratedOption: DatabaseGeneratedOption.None)]
+	public DateTimeOffset UpdateDateTime { get; set; }
+
+	/// <summary>
+	/// تاریخ و زمان شروع انتشار
+	/// </summary>
+	//[System.ComponentModel.DataAnnotations.Display
+	//	(ResourceType = typeof(Resources.DataDictionary),
+	//	Name = nameof(Resources.DataDictionary.PublishStartDateTime))]
+	public System.DateTimeOffset? PublishStartDateTime { get; set; }
+
+	/// <summary>
+	/// تاریخ و زمان پایان انتشار
+	/// </summary>
+	//[System.ComponentModel.DataAnnotations.Display
+	//	(ResourceType = typeof(Resources.DataDictionary),
+	//	Name = nameof(Resources.DataDictionary.PublishFinishDateTime))]
+	public System.DateTimeOffset? PublishFinishDateTime { get; set; }
+}
